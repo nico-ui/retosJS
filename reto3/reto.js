695ffd315b0cfdea35637f6b75a116eb0cb633c5
Nicolai Hernández <cristian.nicolai@outlook.es>
2022-08-18 16:52:35 -0500

    arreglo de objetos

--- reto3/reto.js
+++ reto3/reto.js
@@ -3,12 +3,15 @@ var auto = {
     modelo: "Carola",
     annio: "2022",
 }
+var autos = [];
 function autoConstructor(marca, modelo, annio) {
     this.marca = marca;
     this.modelo = modelo;
     this.annio = annio;
 }
 for (var i = 0; i < 3; i++) {
-    var miauto = new autoConstructor('mini', 'cooper', '201' + i);
-    miauto;
+    autos.push(new autoConstructor('mini', 'cooper', '201' + i));
+}
+for(var i=0; i<3; i++){
+    autos[i];
 }
